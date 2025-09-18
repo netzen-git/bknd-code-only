7f69a1c5285dce9769810deca32bb5695ad41fe3
addastro <github@addastro.com>
2025-09-18 15:52:06 +0200

    Summary

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,18 @@ import { App } from "bknd";
  * We could simply return the `serve` function, but since we're using code,
  * there are no race conditions that apply to db-stored config.
  *
- * Therefore we use the other approach of reusing the app across requests in production.
+ * There fore we use the other approach of reusing the app across requests in production.
  */
 //export default serve(config);
 
 let app: App;
 export default {
-   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-      const prod = env.ENVIRONMENT !== "development";
-      if (!prod || !app) {
-         app = await createApp(config, { request, env, ctx });
-      }
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    const prod = env.ENVIRONMENT !== "development";
+    if (!prod || !app) {
+      app = await createApp(config, { request, env, ctx });
+    }
 
-      return app.fetch(request, env, ctx);
-   },
+    return app.fetch(request, env, ctx);
+  },
 };
